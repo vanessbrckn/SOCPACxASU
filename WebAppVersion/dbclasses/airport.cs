63b002c31523f0cce8d280a049e96546399ef7f0
jlmcnei4 <72997731+jlmcnei4@users.noreply.github.com>
2021-04-12 17:50:51 -0700

    Original made into a Web app (2)
    Moved project to Web App version to add Use Current Location

---
+++ WebAppVersion/dbclasses/airport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper; 
+
+namespace DashboardApp.dbclasses
+{
+    public class airport
+    {
+        public int aid { get; set; }
+        public int capacity { get; set; }
+        public string location { get; set; }
+
+    }
+}
---
+++ WebAppVersion/dbclasses/plane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DashboardApp.dbclasses
+{
+    class plane
+    {
+        public int capacity { get; set; }
+        public int cew_size { get; set; }
+        public string firepower { get; set; }
+        public string licenses { get; set; }
+        public string location { get; set; }
+        public string modelNum { get; set; }
+        public int PlaneID { get; set; }
+
+    }
+}
